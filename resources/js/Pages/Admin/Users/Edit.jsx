a29011e3a47d48bb280a8cb73675326a15963aa5
Владимир Мерзляков <lime0177@outlook.com>
2025-11-10 05:40:37 +0500

    Add group news

--- resources/js/Pages/Admin/Users/Edit.jsx
+++ resources/js/Pages/Admin/Users/Edit.jsx
@@ -30,8 +30,6 @@ export default function UserEdit({ user = null, availableRoles = {} }) {
         </h2>
         <Link
           href={route('admin.admin.users')}
-        <Link
-          href={route('admin.admin.users')}
           className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
           <svg className="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
